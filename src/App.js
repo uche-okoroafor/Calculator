9288aa6cbaf3bad1065acc7cf09060fb64ae34f1
Uche Okoroafor <71725892+uche-okoroafor@users.noreply.github.com>
2020-12-29 20:56:43 +0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React,{component} from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import { evaluate } from 'mathjs'; 
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default App;
+
+
